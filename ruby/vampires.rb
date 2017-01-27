0183fca348393973714130596ccfe19a060891a5
Dillon Arevalo <dillonbarevalo@gmail.com>
2017-01-27 13:43:39 -0800

    Add vampire detection questions but not logic yet

---
+++ ruby/vampires.rb
@@ -0,0 +1,11 @@
+puts "What's your name?"
+name=gets.chomp
+puts "How old are you?"
+age=gets.chomp
+puts "What year were you born?"
+birth_year=gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes/no)"
+bread=gets.chomp
+puts "Would you like to enroll in the company’s health insurance? (yes/no)"
+insurance=gets.chomp
+
