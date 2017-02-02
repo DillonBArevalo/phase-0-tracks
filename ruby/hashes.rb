a3171d919a5ad8307c12ecb356b3fb21907ead68
Dillon Arevalo <dillonbarevalo@gmail.com>
2017-02-01 19:48:25 -0800

    Add rest of questions and the complex would you like to change anything if clause

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -43,4 +43,50 @@ phone.delete!(" ")
 phone=phone.to_i
 client_info[:phone_number]=phone
 
+puts "Do you have any pets?"
+pets=gets.chomp.downcase
+if pets=="yes" || pets=="y"
+  pets=true
+else
+  pets=false
+end
+client_info[:pets_y_n]=pets
+
+puts "At what email can we reach you?"
+email=gets.chomp
+client_info[:email]=email
+
+puts "Is there anything else you want us to know?"
+extra=gets.chomp
+client_info[:additional_info]=extra
+
+puts client_info
+
+puts "Would you like to update any of this data? (enter they key of the data to be changed or none if you don't want to change anything)"
+change_key=gets.chomp
+
+if change_key != "none" #if clause for replacing data type
+  if change_key[0]==":" #if clause for converting change_key to sym
+    change_key = change_key[1..-1].intern
+  else
+    change_key = change_key.intern
+  end
+  puts "What would you like the new data to be?"
+  new_value=gets.chomp
+  if change_key == :pets_y_n #if clause for making sure we get the right data type
+    if new_value.downcase=="yes" || new_value.downcase=="y"
+      new_value=true
+    else
+      new_value=false
+    end
+  elsif change_key == :age || change_key == :number_of_children || change_key == :phone_number #this elsif is for number data types
+    new_value.delete!("-")
+    new_value.delete!(".")
+    new_value.delete!(" ")
+    #the above three lines are to make sure it's in the right format (just a string of numbers)
+    new_value=new_value.to_i
+  end
+  client_info[change_key] = new_value
+end
+
 puts client_info
