cea0e5e5533bfb0768d9a3ea94282176799bc056
Dillon Arevalo <dillonbarevalo@gmail.com>
2017-02-01 17:21:25 -0800

    Enter answer for question 1

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,7 +4,8 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+ "iNvEsTiGaTiOn".swapcase
+ "iNvEsTiGaTiOn".swapcase!
 # => “InVeStIgAtIoN”
 
 # "zom".<???>
