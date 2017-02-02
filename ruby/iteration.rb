906ef76dbbcd61a7b3b38bbbb2eb5ec79626f8bc
Dillon Arevalo <dillonbarevalo@gmail.com>
2017-02-01 20:39:25 -0800

    Initialize array and hash, make method at opening, and run each on both of them

---
+++ ruby/iteration.rb
@@ -0,0 +1,31 @@
+def practice
+  param1 = "This"
+  param2 = "That"
+  puts "What would you like"
+  yield(param1, param2)
+end
+
+# practice { |param1, param2| puts "I want #{param1} AND #{param2}"}
+
+arr =["carrot", "cucumber", "apple", "beef"]
+
+h ={
+  carrot: "vegetable",
+  cucumber: "vegetable",
+  apple: "fruit",
+  beef: "meat"
+}
+
+puts "Original array:"
+puts arr
+puts "Original hash:"
+puts h
+
+arr.each {|food_item| puts "I like to eat #{food_item}"}
+
+h.each {|food_item, food_type| puts "#{food_item} is #{food_type}"}
+
+puts "Post each array:"
+puts arr
+puts "Post each hash:"
+puts h
