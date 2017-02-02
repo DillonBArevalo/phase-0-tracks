9452b66fdd8385227aa7d91c25abfa69855add7e
Dillon Arevalo <dillonbarevalo@gmail.com>
2017-02-01 19:11:04 -0800

    Add pseudocode for assignment 2

---
+++ ruby/hashes.rb
@@ -0,0 +1,10 @@
+=begin
+Create hash with all the desired fields set to nil
+loop through the hash asking for data to enter
+use an until loop with until being length of the hash and iterate through indicies using hash.keys[index]
+print the hash using puts
+ask for update of key
+change the key if not none
+print hash again
+=end
+
