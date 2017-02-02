2ba7cffb35c68582c2e776441028d2bccf9b014a
Dillon Arevalo <dillonbarevalo@gmail.com>
2017-02-01 19:20:54 -0800

    Add client info hash without information and first two questions with inputs

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,10 +1,29 @@
 =begin
 Create hash with all the desired fields set to nil
-loop through the hash asking for data to enter
-use an until loop with until being length of the hash and iterate through indicies using hash.keys[index]
+Ask for each data point separately as looping won't easily work because we'll want different questions for each one
 print the hash using puts
 ask for update of key
 change the key if not none
 print hash again
 =end
 
+client_info = {
+  name: nil, 
+  age: nil, 
+  number_of_children: nil, 
+  decor_theme: nil,
+  phone_number: nil,
+  pets_y_n: nil,
+  email: nil,
+  additional_info: nil
+}
+
+puts "What is your name?"
+name=gets.chomp
+client_info[:name] = name
+
+puts "How old are you?"
+age=gets.chomp.to_i
+client_info[:age]=age
+
+puts client_info
