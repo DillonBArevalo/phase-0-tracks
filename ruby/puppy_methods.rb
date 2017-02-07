03d6b9ff8546b1410882338d857e993724ad10d6
Dillon Arevalo <dillonbarevalo@gmail.com>
2017-02-07 14:37:07 -0800

    Add puppy methods and call them. add cat class with methods and initialize a cat storing list

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,50 @@ class Puppy
     toy
   end
 
+  def speak(int)
+    int.times {|num| puts "Woof!"}
+  end
+
+  def roll_over
+    puts "Rolls over"
+  end
+
+  def dog_years(int)
+    int*7 #should implicitly return this
+  end
+
+  def play_dead
+    puts "Plays dead (awwwww)"
+  end
+
+  def initialize
+    puts "Initializing new puppy instance ..."
+  end
+
 end
+
+frank = Puppy.new
+
+frank.fetch("ball")
+frank.speak(5)
+frank.roll_over
+frank.play_dead
+
+class Cat
+
+  def initialize
+    puts "Initializing new cat instance..."
+  end
+
+  def purr
+    puts "purrs"
+  end
+
+  def hiss
+    puts "hisses"
+  end
+
+end
+
+cat_list = Array.new
 
