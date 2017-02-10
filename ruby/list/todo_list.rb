843b87e3a2d22a20f18267e78fe1f26a4ebf9f57
Dillon Arevalo <dillonbarevalo@gmail.com>
2017-02-10 12:29:14 -0800

    make get_items work

---
+++ ruby/list/todo_list.rb
@@ -0,0 +1,21 @@
+class TodoList
+
+  def initialize(tasks)
+    @list = tasks
+  end
+
+  def get_items
+    @list
+  end
+
+  def add_item(item)
+  end
+
+  def delete_item(item)
+  end
+
+  def get_item(item)
+  end
+
+end
+
