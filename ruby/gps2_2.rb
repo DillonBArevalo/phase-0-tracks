00bdc0bcaa728b2d7371fd7b5f40661d6ee4c5a8
Dillon Arevalo <dillonbarevalo@gmail.com>
2017-02-09 17:23:48 -0800

    Psuedocode and add make list method with limited functinoality

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,38 @@
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # Create an empty hash
+  # separate string into an array of individual items
+  # make each one a key for a hash with a default quantity
+  # print the list to the console [can you use one of your other methods here?]
+# output: A hash of the items with default quantities
+
+def make_list(item_string)
+  grocery_list = {}
+  items_array = item_string.split(" ")
+  #puts items_array
+end
+
+#make_list("carrots apples cereal pizza")
+
+
+# Method to add an item to a list
+# input: list, item name, and optional quantity
+# steps:
+  # make item name
+# output:
+
+# Method to remove an item from the list
+# input:
+# steps:
+# output:
+
+# Method to update the quantity of an item
+# input:
+# steps:
+# output:
+
+# Method to print a list and make it look pretty
+# input:
+# steps:
+# output:
