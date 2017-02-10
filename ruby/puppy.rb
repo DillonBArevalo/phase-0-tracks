9c838c324014d3c1f88167520b0cd4bf95686c6c
Dillon Arevalo <dillonbarevalo@gmail.com>
2017-02-09 18:54:41 -0800

    Create psuedocode for basic puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,20 @@
+=begin
+SPECIES ------------------------
+.
+
+CHARACTERISTICS ----------------
+fur length: 
+Friendly: 
+tail length: 
+Name: 
+fur color: 
+eye color:
+
+BEHAVIOR -----------------------
+tail wag
+fetch
+swim
+run
+lick
+bark
+=end
