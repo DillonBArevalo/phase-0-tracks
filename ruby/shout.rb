0a83baf50ca619a7ab8ab6b13cdc44dc1112769c
Dillon Arevalo <dillonbarevalo@gmail.com>
2017-02-09 22:53:01 -0800

    Make mixin version and test it with pirates and jerks

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,4 @@
+=begin
 module Shout
 
   def self.yell_angrily(words)
@@ -10,11 +11,38 @@ module Shout
 
 end
 
+=end
 
+module Shout
 
+  def yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def yell_happily(words)
+    words.upcase+"!"+" :D"
+  end
+
+end
+
+class Pirate
+  include Shout
+end
+
+class Jerk
+  include Shout
+end
 
 #==================Driver Code====================
 
 
-puts Shout.yell_happily("yaaaaay")
-puts Shout.yell_angrily("noooooooo")
+#puts Shout.yell_happily("yaaaaay")
+#puts Shout.yell_angrily("noooooooo")
+
+blackbeard = Pirate.new
+hurk = Jerk.new
+
+puts blackbeard.yell_happily("Yaaarrrrrr, Land Ho!")
+puts blackbeard.yell_angrily("Avast, we be taking enemy fire!")
+puts hurk.yell_angrily("Hey weakling! Get over here!")
+puts hurk.yell_happily("Yaaay! Candy!")
