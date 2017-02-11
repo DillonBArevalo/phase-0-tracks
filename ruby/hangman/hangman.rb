7d465d5d98fbae1f919b90667e685f85e0785412
Dillon Arevalo <dillonbarevalo@gmail.com>
2017-02-10 22:29:12 -0800

    Make initial empty functions for class Hangman and require it in the spec file.

---
+++ ruby/hangman/hangman.rb
@@ -0,0 +1,23 @@
+=begin
+make class h
+=end
+
+class Hangman
+
+  def initialize
+    @answer = ""
+    @current_state = ""
+    @guesses = 1
+    @letters_guessed = []
+  end
+
+  def is_over
+  end
+
+  def set_answer(answer)
+  end
+
+  def guess_letter(letter)
+  end
+
+end
