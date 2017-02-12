a66616e67053dd47898afbda73ad7cb221e84923
Dillon Arevalo <dillonbarevalo@gmail.com>
2017-02-12 14:35:04 -0800

    Add many rspec tests and pseudocode and method starts and ends.

--- ruby/hangman/hangman.rb
+++ ruby/hangman/hangman.rb
@@ -1,8 +1,26 @@
 =begin
-make class h
+make class hangman
+i'll need a method for inputting the answer to be guessed at
+I'll want this to accept a string and generate both the answer attribute and a current guesses attribute
+I'll want a method to count guesses and an attribute to hold guesses
+When I start the game I'll also need a metric for deciding how many guesses to give
+when guesses get to zero display the game lost message (check for winning first) 
+
+to simplify things i'll create these sub-methods:
+  update current state (will change current state to match answer as much as it should given the guesses)
+  update guesses (updates guesses and checks that current guess hasn't been guessed yet)
+
+guess numbers: 
+  4 or fewer letters, 10 guesses
+  5-6 letters, 9 guesses
+  7-8 letters, 8 guesses
+  9-10 letters, 7
+  11 or more letters, 6 guesses
 =end
 
 class Hangman
+
+  attr_reader :answer, :current_state, :guesses, :letters_guessed
 
   def initialize
     @answer = ""
@@ -14,7 +32,13 @@ class Hangman
   def is_over
   end
 
-  def set_answer(answer)
+  def update_guesses(letter)
+  end
+
+  def update_current_state()
+  end
+
+  def start_game(answer)
   end
 
   def guess_letter(letter)
--- ruby/hangman/hangman_spec.rb
+++ ruby/hangman/hangman_spec.rb
@@ -1,2 +1,48 @@
 require_relative "hangman"
 
+describe Hangman do
+  let(:game) {Hangman.new}
+
+  it "takes a guess and creates answer and guess keys" do
+    game.start_game("example")
+    expect(game.answer).to eq ["example"]
+    expect(game.current_state).to eq ["-------"]
+  end
+
+  it "can initialize guess numbers correctly" do
+    game.start_game("ten")
+    expect(game.guesses).to eq [10]
+    game.start_game("nines")
+    expect(game.guesses).to eq [9]
+    game.start_game("eightgs")
+    expect(game.guesses).to eq [8]
+    game.start_game("sevenguess")
+    expect(game.guesses).to eq [7]
+    game.start_game("sixguessesg")
+    expect(game.guesses).to eq [6]
+  end
+
+  it "can accept an answer with space(s)" do
+    game.start_game("example game answer")
+    expect(game.answer).to eq ["example game answer"]
+    expect(game.current_state).to eq ["------- ---- ------"]
+  end
+
+  it "takes a correct guess, updates the current state, guesses and letters guessed appropriately" do
+    game.start_game("example")
+    game.guess_letter("e")
+    expect(game.current_state).to eq ["e-----e"]
+    expect(game.guesses).to eq [8]
+    game.guess_letter("0")
+    expect(game.current_state).to eq ["e-----e"]
+    expect(game.guesses).to eq [7]
+  end
+
+  it "takes an incorrect guess, doesn't change the current state, updates guesses and letters guessed appropriately" do
+    game.start_game("example")
+
+  end
+
+
+
+end
