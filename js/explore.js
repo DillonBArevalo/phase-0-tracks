31b949f12aa2e588252d72a70bcb94a2d2314989
Dillon Arevalo <dillonbarevalo@gmail.com>
2017-02-15 16:35:33 -0800

    Add file and pseudocode for reversing strings

---
+++ js/explore.js
@@ -0,0 +1,1 @@
+// function should take a string, set another empty string. iterate backwards through the letters of the input string and one by one add them to the new empty string. then return the no longer empty string
