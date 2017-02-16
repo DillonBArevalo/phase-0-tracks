07a286f89d190adc043083de75af9f2149ad7836
Dillon Arevalo <dillonbarevalo@gmail.com>
2017-02-16 12:37:44 -0800

    Add car constructor and a loop to create ten cars with random attributes

--- js/data_structures.js
+++ js/data_structures.js
@@ -7,7 +7,21 @@ names.push("Rolf");
 
 //Math.floor(Math.random() * (colors.length)) creates a random number between zero and length of array-1 (possible indices)
 var horses = {};
-horses[names[Math.floor(Math.random() * (colors.length))]] = colors[Math.floor(Math.random() * (colors.length))];
-console.log(horses);
+horses[names[Math.floor(Math.random() * (names.length))]] = colors[Math.floor(Math.random() * (colors.length))];
+// console.log(horses);
 
-Math.floor(Math.random() * (colors.length));
+var models = ["Model-T", "CR-V", "Civic", "Prius", "Explorer"];
+
+function Car(color, model, nickname){
+  this.color = color;
+  this.model = model;
+  this.nickname = nickname;
+  this.drive = function() { console.log("*Vroom vroom!*"); };
+}
+
+
+for (i=0; i<10; i++){
+  var car = new Car(colors[Math.floor(Math.random() * (colors.length))], models[Math.floor(Math.random() * (models.length))], names[Math.floor(Math.random() * (names.length))]);
+  car.drive();
+  console.log(car);
+}
