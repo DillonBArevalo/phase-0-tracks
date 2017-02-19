2f231f0092b85d93c787e653be84fa3fecdfe4d9
Dillon Arevalo <dillonbarevalo@gmail.com>
2017-02-18 21:51:58 -0800

    Pseudocode and code key value match checker

--- js/algos.js
+++ js/algos.js
@@ -13,6 +13,26 @@ function longest_phrase(phrases) {
   return longest;
 };
 
+/*
+For key value match:
+create a list of the keys in each object. 
+iterate through keys on one, then in that iteration, iterate through the other. 
+if i find a match, check the values. if they match return true.
+*/
+
+function key_value_match(obj1, obj2) {
+  var keys1 = Object.keys(obj1);
+  var keys2 = Object.keys(obj2);
+  for (var i = 0; i < keys1.length; i++) {
+    for (var j = 0; j < keys2.length; j++) {
+      if (keys1[i] == keys2[j]) {
+        if (obj1(keys1[i]) == obj2(keys2[j])) {
+          return true;
+        };
+      };
+    };
+  };
+};
 
 
 //============Driver Code=============
